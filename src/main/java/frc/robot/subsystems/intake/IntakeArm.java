625c8cf7671e9fc39551ed1ede8fd2e2d8aa07b7
biqsweet <ranranlevyy@gmail.com>
2024-10-22 16:59:19 +0300

    Feat: can now engage and disengage IntakeArm

--- src/main/java/frc/robot/subsystems/intake/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeArm.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.generic.GenericSubsystem;
@@ -10,11 +9,14 @@ import static frc.robot.subsystems.intake.IntakeArmConstants.INTAKE_ARM_MOTOR;
 
 
 public class IntakeArm extends GenericSubsystem {
-    public Command setIntakeArmPosition(Rotation2d targetPosition) {
-        return Commands.run(() -> setTargetPosition(targetPosition));
+    public Command engageIntakeArm() {
+        return Commands.run(() -> setTargetPosition(true));
+    }
+    public Command disengageIntakeArm() {
+        return Commands.run(() -> setTargetPosition(false));
     }
 
-    private void setTargetPosition(Rotation2d targetPosition) {
-        INTAKE_ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION, targetPosition.getRotations());
+    private void setTargetPosition(boolean engaged) {
+        INTAKE_ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION, engaged ? 0 : 45);
     }
 }
