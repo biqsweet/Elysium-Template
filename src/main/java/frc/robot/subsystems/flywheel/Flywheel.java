d4e8bce49c8d523458485cd0ada5ac64661c716b
biqsweet <ranranlevyy@gmail.com>
2024-10-25 17:58:02 +0300

    Fix: Flywheel setVelocity to Voltage

--- src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
+++ src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
@@ -8,14 +8,14 @@ import static frc.robot.subsystems.flywheel.FlywheelConstants.FLYWHEEL_MOTOR;
 
 public class Flywheel {
     public Command setFlywheelVelocity() {
-        return Commands.run(this::setVelocity);
+        return Commands.run(this::setVoltage);
     }
 
     public Command stop() {
         return Commands.runOnce(FLYWHEEL_MOTOR::stopMotor);
     }
 
-    private void setVelocity() {
-        FLYWHEEL_MOTOR.setOutput(MotorProperties.ControlMode.VELOCITY, 10);
+    private void setVoltage() {
+        FLYWHEEL_MOTOR.setOutput(MotorProperties.ControlMode.VOLTAGE, 10);
     }
 }
