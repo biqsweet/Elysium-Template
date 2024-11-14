e3fcd6b780498202539f9ca4c593fbeedd5625c7
biqsweet <ranranlevyy@gmail.com>
2024-11-14 18:54:46 +0200

    Fix: Flywheel extend GenericSubsystem and can set custom voltages instead of preset

--- src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
+++ src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
@@ -3,24 +3,25 @@ package frc.robot.subsystems.flywheel;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.lib.generic.GenericSubsystem;
 import frc.lib.generic.hardware.motor.MotorProperties;
 
 import static frc.robot.subsystems.flywheel.FlywheelConstants.FLYWHEEL_MOTOR;
 
-public class Flywheel {
-    public Command setFlywheelVelocity() {
-        return Commands.run(this::setVoltage);
+public class Flywheel extends GenericSubsystem {
+    public Command setFlywheelVoltage(double voltage) {
+        return Commands.run(() -> setVoltage(voltage), this);  //todo:  requires wHO?! V
     }
 
     public Command stop() {
-        return Commands.runOnce(FLYWHEEL_MOTOR::stopMotor);
+        return Commands.runOnce(FLYWHEEL_MOTOR::stopMotor, this);  //todo: requirement V
     }
 
     private Rotation2d getCurrentPosition() {
         return Rotation2d.fromRotations(FLYWHEEL_MOTOR.getSystemPosition());
     }
 
-    private void setVoltage() {
-        FLYWHEEL_MOTOR.setOutput(MotorProperties.ControlMode.VOLTAGE, 10);
+    private void setVoltage(double voltage) {
+        FLYWHEEL_MOTOR.setOutput(MotorProperties.ControlMode.VOLTAGE, voltage);  //todo: No. Use variables instead. V
     }
 }
