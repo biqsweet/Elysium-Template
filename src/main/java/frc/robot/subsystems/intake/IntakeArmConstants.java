98a62066c097e127ef5572fb76f6a0776adfb4a3
biqsweet <ranranlevyy@gmail.com>
2024-11-14 19:09:32 +0200

    Fix: IntakeArm kp to 10

--- src/main/java/frc/robot/subsystems/intake/IntakeArmConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeArmConstants.java
@@ -42,8 +42,9 @@ public class IntakeArmConstants {
 
         INTAKE_ARM_MOTOR.setupSignalUpdates(MotorSignal.POSITION);
         INTAKE_ARM_MOTOR.setupSignalUpdates(MotorSignal.VELOCITY);
+        INTAKE_ARM_MOTOR.setupSignalUpdates(MotorSignal.VOLTAGE);
 
-        intakeArmMotorConfiguration.simulationSlot = new MotorProperties.Slot(1, 0, 0, 0, 0, 0);
+        intakeArmMotorConfiguration.simulationSlot = new MotorProperties.Slot(10, 0, 0, 0, 0, 0);
         intakeArmMotorConfiguration.simulationProperties = new SimulationProperties.Slot(SimulationProperties.SimulationType.ARM, DCMotor.getFalcon500(1), 50, 0.5, 0.2, MINIMUM_ANGLE, MAXIMUM_ANGLE, true);
 
         INTAKE_ARM_MOTOR.configure(intakeArmMotorConfiguration);
