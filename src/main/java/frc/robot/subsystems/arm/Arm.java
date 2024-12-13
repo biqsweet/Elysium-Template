0c1d31e2e8afcaff8717f213e7193504a3d9b6f0
biqsweet <ranranlevyy@gmail.com>
2024-12-13 10:46:29 +0200

    Fix: Component3d works spinArm command added, autoAim fixed and get Current and Target Arm Position commands added

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,12 +2,12 @@ package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.generic.GenericSubsystem;
 import frc.lib.generic.hardware.motor.MotorProperties;
-import org.littletonrobotics.junction.Logger;
 
 import static frc.robot.GlobalConstants.HUB_POSITION;
 import static frc.robot.RobotContainer.POSE_ESTIMATOR;
@@ -27,32 +27,35 @@ public class Arm extends GenericSubsystem {
         return Commands.run(() -> ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION, autoAim()), this);
     }
 
+    public Command spinArm() {
+        return Commands.run(() -> ARM_MOTOR.setOutput(MotorProperties.ControlMode.VOLTAGE, 5), this);
+    }
+
     public Command stop() {
         return Commands.runOnce(ARM_MOTOR::stopMotor, this);
     }
 
-    private double autoAim() {
-        final Translation2d robotPose = POSE_ESTIMATOR.getCurrentPose().getTranslation();
-
-        final Translation2d distanceToHub = HUB_POSITION.toTranslation2d().minus(robotPose);
-
-        Logger.recordOutput("DistanceToHub", distanceToHub);
-        Logger.recordOutput("ArmPos", getCurrentArmPosition());
-
-        return Units.radiansToRotations(
-                Math.atan2(
-                        distanceToHub.getNorm(), HUB_POSITION.getZ() - 1
-                )
-        );
-    }
-
     @Override
     public void periodic() {
-        ARM_POSE_3D.updateComponent(getCurrentArmPosition(), TURRET.getCurrentTurretPosition());
+        ARM_POSE_3D.updateComponent(getCurrentArmPosition().plus(Rotation2d.fromRotations(0.15)), TURRET.getCurrentTurretPosition(), new Translation3d(0, 0, 0));
     }
 
     public Rotation2d getCurrentArmPosition() {
         return Rotation2d.fromRotations(ARM_MOTOR.getSystemPosition());
     }
 
+    public Rotation2d getTargetArmPosition() {
+        return Rotation2d.fromRotations(ARM_MOTOR.getClosedLoopTarget());
+    }
+
+    private double autoAim() {
+        final Translation2d robotPose = POSE_ESTIMATOR.getCurrentPose().getTranslation();
+        final Translation2d distanceToHub = HUB_POSITION.toTranslation2d().minus(robotPose);
+
+        return Units.radiansToRotations(
+                Math.atan2(
+                        distanceToHub.getNorm(), HUB_POSITION.getZ() - 1
+                ) + Units.degreesToRadians(10)
+        );
+    }
 }
