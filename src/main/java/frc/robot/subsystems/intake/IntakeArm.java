73ac5dd586e065cf3d12dcf4a33d4ac12ea0312e
biqsweet <ranranlevyy@gmail.com>
2024-12-13 10:44:58 +0200

    Fix: Component3d works and spinIntakeArm Command added

--- src/main/java/frc/robot/subsystems/intake/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeArm.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.generic.GenericSubsystem;
@@ -17,24 +18,20 @@ public class IntakeArm extends GenericSubsystem {
         return Commands.run(() -> INTAKE_ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION, position), this);
     }
 
+    public Command spinIntakeArm() {
+        return Commands.run(() -> INTAKE_ARM_MOTOR.setOutput(MotorProperties.ControlMode.VOLTAGE, 5), this);
+    }
+
     public Command stop() {
         return Commands.runOnce(INTAKE_ARM_MOTOR::stopMotor, this);
-    } //todo req V
+    }
 
     @Override
     public void periodic() {
-        INTAKE_ARM_POSE_3D.updateComponent(getCurrentIntakeArmPosition(), INTAKE_ARM_YAW);
+        INTAKE_ARM_POSE_3D.updateComponent(getCurrentIntakeArmPosition(), INTAKE_ARM_YAW, new Translation3d(0, 0, 0));
     }
-    //todo: instead of initializing a new rotation2d object every loop, pass a predefined rotation2d object with the value of 0 V
 
     private Rotation2d getCurrentIntakeArmPosition() {
         return Rotation2d.fromRotations(INTAKE_ARM_MOTOR.getSystemPosition());
     }
-
-//    /**
-//     * @param position in rotations
-//     */
-//    private void setTargetPosition(double position) {
-//        INTAKE_ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION, position); //todo get a double not a boolean V
-//    }
 }
