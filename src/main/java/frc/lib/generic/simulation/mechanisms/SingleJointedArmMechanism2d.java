9c9317ab2a0fe9950dccf49bcef5f0255b2d1d4a
biqsweet <ranranlevyy@gmail.com>
2024-12-15 21:12:38 +0200

    Feat: added easy single jointed arm Mechanism2d visualization and creation

---
+++ src/main/java/frc/lib/generic/simulation/mechanisms/SingleJointedArmMechanism2d.java
@@ -0,0 +1,32 @@
+package frc.lib.generic.simulation.mechanisms;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import org.littletonrobotics.junction.Logger;
+
+
+public class SingleJointedArmMechanism2d {
+    private final Mechanism2d armMechanism;
+    private final MechanismLigament2d armLigament;
+
+    public SingleJointedArmMechanism2d(double armLengthMeters, Rotation2d minimumAngle, Rotation2d maximumAngle) {
+        this.armMechanism = new Mechanism2d(10, 10);
+
+        MechanismRoot2d root = armMechanism.getRoot("armMechanism", 5, 5);
+        this.armLigament = new MechanismLigament2d("armLigament", armLengthMeters, minimumAngle.getDegrees(), 3, new Color8Bit(0, 0, 255));
+        root.append(armLigament);
+    }
+
+    public void updateMechanism(Rotation2d newAngle) {
+        armLigament.setAngle(newAngle);
+        Logger.recordOutput("armMechanism2d", armMechanism);
+    }
+
+    public Mechanism2d getMechanism() {
+        return armMechanism;
+    }
+
+}
