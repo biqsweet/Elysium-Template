352ae52180c095cbe7d872b479bc62f6f7026423
biqsweet <ranranlevyy@gmail.com>
2024-12-17 12:44:49 +0200

    Fix: single jointed arm Mechanism2d target visualization

--- src/main/java/frc/lib/generic/simulation/mechanisms/SingleJointedArmMechanism2d.java
+++ src/main/java/frc/lib/generic/simulation/mechanisms/SingleJointedArmMechanism2d.java
@@ -11,17 +11,21 @@ import org.littletonrobotics.junction.Logger;
 public class SingleJointedArmMechanism2d {
     private final Mechanism2d armMechanism;
     private final MechanismLigament2d armLigament;
+    private final MechanismLigament2d targetAngle;
 
     public SingleJointedArmMechanism2d(double armLengthMeters, Rotation2d minimumAngle, Rotation2d maximumAngle) {
         this.armMechanism = new Mechanism2d(10, 10);
 
         MechanismRoot2d root = armMechanism.getRoot("armMechanism", 5, 5);
         this.armLigament = new MechanismLigament2d("armLigament", armLengthMeters, minimumAngle.getDegrees(), 3, new Color8Bit(0, 0, 255));
+        this.targetAngle = new MechanismLigament2d("targetAngle", armLengthMeters, minimumAngle.getDegrees(), 3, new Color8Bit(144, 144, 144));
         root.append(armLigament);
+        root.append(targetAngle);
     }
 
-    public void updateMechanism(Rotation2d newAngle) {
+    public void updateMechanism(Rotation2d newAngle, Rotation2d newTargetAngle) {
         armLigament.setAngle(newAngle);
+        targetAngle.setAngle(newTargetAngle);
         Logger.recordOutput("armMechanism2d", armMechanism);
     }
 
