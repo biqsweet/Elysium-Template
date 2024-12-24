7232c796554eea17e1f7e2ef99805c770ca64404
biqsweet <ranranlevyy@gmail.com>
2024-12-24 13:54:48 +0200

    Fix: added some objects to GlobalConstants and RobotContainer

--- src/main/java/frc/robot/GlobalConstants.java
+++ src/main/java/frc/robot/GlobalConstants.java
@@ -3,11 +3,14 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class GlobalConstants {
+    public static final BuiltInAccelerometer ROBORIO_ACCELEROMETER = new BuiltInAccelerometer();
+
     public static final double ROBOT_PERIODIC_LOOP_TIME = 0.02;
 
     public static final double ODOMETRY_FREQUENCY_HERTZ = 200.0;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,23 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.util.Controller;
 import frc.robot.commands.ShootBall;
+import frc.robot.poseestimation.objectdetection.DetectionCameraFactory;
+import frc.robot.poseestimation.objectdetection.DetectionCameraIO;
 import frc.robot.poseestimation.poseestimator.PoseEstimator;
 import frc.robot.subsystems.SimulationTest.*;
+import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.IntakeArm;
 import frc.robot.subsystems.swerve.Swerve;
+import frc.robot.subsystems.swerve.SwerveCommands;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.verticalconveyor.VerticalConveyor;
 
@@ -25,17 +32,25 @@ import static frc.robot.poseestimation.poseestimator.PoseEstimatorConstants.FRON
 
 public class RobotContainer {
     public static final PoseEstimator POSE_ESTIMATOR = new PoseEstimator(FRONT_CAMERA);
-    public static final Swerve SWERVE = new Swerve();
+
+    public static final DetectionCameraIO DETECTION_CAMERA = DetectionCameraFactory.createDetectionCamera("NotesCamera",
+            new Transform3d(
+                    new Translation3d(0.3, 0.08, 0.31),
+                    new Rotation3d()
+            ));
+
     public static final BuiltInAccelerometer ROBORIO_ACCELEROMETER = new BuiltInAccelerometer();
-    public static final frc.robot.subsystems.arm.Arm ARM = new frc.robot.subsystems.arm.Arm();
-    public static final frc.robot.subsystems.flywheel.Flywheel FLYWHEEL = new frc.robot.subsystems.flywheel.Flywheel();
+
+    public static final Arm ARM = new Arm();
+    public static final Flywheel FLYWHEEL = new Flywheel();
+    public static final Swerve SWERVE = new Swerve();
     public static final Intake INTAKE = new Intake();
     public static final IntakeArm INTAKE_ARM = new IntakeArm();
     public static final ShootBall SHOOT_BALL = new ShootBall();
     public static final Turret TURRET = new Turret();
     public static final VerticalConveyor CONVEYOR = new VerticalConveyor();
 
-    public static final Arm armSim = new Arm();
+    public static final ArmSim armSim = new ArmSim();
     public static final Flywheel flywheelSim = new Flywheel();
     public static final DoubleJointedArm doubleSim = new DoubleJointedArm();
     public static final Elevator elevatorSim = new Elevator();
@@ -54,27 +69,22 @@ public class RobotContainer {
         //B=X on keyboard
         //X=C on keyboard
         //Y=V on keyboard
-        driveController.getButton(Controller.Inputs.A).onTrue(flywheelSim.setFlywheelVoltage(4));
-        driveController.getButton(Controller.Inputs.B).onTrue(flywheelSim.setFlywheelVoltage(10));
-        driveController.getButton(Controller.Inputs.X).onTrue(flywheelSim.setFlywheelVoltage(-10));
-        driveController.getButton(Controller.Inputs.Y).onTrue(flywheelSim.setFlywheelVoltage(-4));
+        driveController.getButton(Controller.Inputs.A).onTrue(flywheelSim.setFlywheelVoltage(40));
+        driveController.getButton(Controller.Inputs.B).onTrue(flywheelSim.setFlywheelVoltage(100));
+        driveController.getButton(Controller.Inputs.X).onTrue(flywheelSim.setFlywheelVoltage(-100));
+        driveController.getButton(Controller.Inputs.Y).onTrue(flywheelSim.setFlywheelVoltage(-40));
 
         DoubleSupplier translationSupplier = () -> MathUtil.applyDeadband(-driveController.getRawAxis(LEFT_Y), 0.05);
         DoubleSupplier strafeSupplier = () -> MathUtil.applyDeadband(-driveController.getRawAxis(LEFT_X), 0.05);
         DoubleSupplier turningSupplier = () -> MathUtil.applyDeadband(-driveController.getRawAxis(RIGHT_X), 0.05);
 
         SWERVE.setDefaultCommand(
-                SWERVE.driveOpenLoop(
+                SwerveCommands.driveOpenLoop(
                         translationSupplier,
                         strafeSupplier,
-                        turningSupplier,
-                        () -> driveController.getStick(Controller.Stick.RIGHT_STICK).getAsBoolean()
-                ));
 
-        driveController.getButton(Controller.Inputs.START).whileTrue(SWERVE.resetGyro());
-        driveController.getButton(Controller.Inputs.BACK).whileTrue(SWERVE.lockSwerve());
-
-
+                        () -> -driveController.getRawAxis(Controller.Axis.RIGHT_X) * 6,
+                        () -> driveController.getStick(Controller.Stick.RIGHT_STICK).getAsBoolean()));
     }
 
 //    private void setupCharacterization(GenericSubsystem subsystem) {
